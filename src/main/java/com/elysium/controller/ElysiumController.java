c57dabe5c9fd8f2c79396b4db53fbce83243bed8
CodeAB97 <abhishake6097@gmail.com>
2024-01-27 21:12:15 +0530

    COLL-1 : Change name 1 revert

--- src/main/java/com/elysium/controller/ElysiumController.java
+++ src/main/java/com/elysium/controller/ElysiumController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ElysiumController {
 
     @GetMapping("/message")
-    public HelloWorld getMessages() {
+    public HelloWorld getMessage() {
         return new HelloWorld("Hello World from Elysium");
     }
 
